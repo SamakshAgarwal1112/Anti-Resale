273e083e580fbb0b6fb3af064f8fe07e52a90885
Juhi-Pant <pantjuhi2009@gmail.com>
2024-11-10 06:56:59 +0530

    Revert "first"
    This reverts commit 9206cac53de83d2c4ff117a01319958e4aa693aa.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        WEB3_CLIENT_ID: process.env.WEB3_CLIENT_ID,
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
